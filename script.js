f84a36283f7f9f652926ee7e58ec23877885f67b
koverda <koverda@gmail.com>
2016-02-24 09:47:07 -0800

    it kinda works!

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
+  document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    renderTimerForm();
+  });
 
 
-function myFunction() {
+function renderTimerForm() {
+document.getElementById('timerForm').innerHTML = `<form onsubmit="startTimerFunction(); return false;">
+        timerLength: <input type="text" name="timerLength" value="10">
+        <br>
+        breakLength: <input type="text" name="breakLength" value="1">
+        <br>
+        <input type="submit" value="startTimer">
+    </form>`;
+}
+
+function clearTimerForm() {
+document.getElementById('timerForm').innerHTML = ``;
+}
+
+
+function startTimerFunction() {
 	var timerLength = parseInt(document.getElementsByName("timerLength")[0].value);
 	var breakLength = parseInt(document.getElementsByName("breakLength")[0].value);
-    //window.alert("timerLength:" + timerLength);
 
-    document.getElementById('timerDisplay').innerHTML = timerLength;
+  clearTimerForm();
+  document.getElementById('timerDisplay').innerHTML = `workTime <br> <div id="timerDisplayClock">` + timerLength +`</div>`;
 
-console.log(typeof timerLength);
-console.log(typeof breakLength);
+  console.log(typeof timerLength);
+  console.log(typeof breakLength);
 
-    var seconds_left = timerLength;
+  var seconds_left = timerLength;
 
 	var interval = setInterval(function() {
-	    document.getElementById('timerDisplay').innerHTML = --seconds_left;
+	    document.getElementById('timerDisplayClock').innerHTML = --seconds_left;
 
 	    if (seconds_left <= 0)
 	    {
-	        document.getElementById('timerDisplay').innerHTML = 'You are ready';
+	        document.getElementById('timerDisplay').innerHTML = `
+            break time! <br> 
+            aw yeah    
+            <form onsubmit="myFunction(); return false;"> 
+              <input type="submit" value="restartTimer">
+            </form>
+            `;
 	        clearInterval(interval);
 	        notifyMe();
 	    }
@@ -45,5 +70,4 @@ function notifyMe() {
     };
 
   }
-
 }
